ede601222d24b3547ae8bcbb9a80dd05c988dfcd
Trevor Hosek <trevorhosek@gmail.com>
2019-12-08 19:13:54 -0500

    made project compatable with i101 folder

--- main.js
+++
@@ -1,8 +0,0 @@
-function arrow(){
-    var image = document.getElementById("datavizImage")
-    if (image.src.match("i101_viz1.png")){
-        image.src = "i101_viz2.png";
-    } else {
-        image.src = "i101_viz1.png"
-    }
-}
